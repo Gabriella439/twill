a6902316bb26e4addd72cfc4744f47918bdc2e7b
Joseph Abrahamson <me@jspha.com>
2012-11-07 17:02:45 -0800

    Added request signing

--- src/Service/Twilio.hs
+++ src/Service/Twilio.hs
@@ -5,15 +5,42 @@
 -- which are currently exposed by "Service.Twilio.Types" that aren't
 -- exposed here.
 module Service.Twilio (
-  Price,
+  -- * Base Twilio types
+  Price, -- type synonym for 'Int'
   Passport (..),
   PhoneNumber (..),
   SendStatus (..),
   APIKind (..),
   SMSKind (..),
   Id (..), uri,
-  SMS (..)
+  SMS (..),
+  -- * Request signing
+  requestSignature
   ) where
 
 import Prelude hiding (id)
 import Service.Twilio.Types
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Base64 as B64
+import Data.Monoid
+import Data.List
+import Data.Ord
+
+import Crypto.Hash.SHA1 (hash)
+import Crypto.MAC.HMAC (hmac)
+
+
+-- | Given a Passport, a target URL, the raw query string, and a set
+-- of body parameters, this function computes the canonical request
+-- signature Twilio uses to authenticate itself.
+requestSignature :: Passport
+                    -> ByteString -- ^ The full URL
+                    -> ByteString -- ^ The raw query string including the "?"
+                    -> [(ByteString, ByteString)] -- ^ Post parameters in Body
+                    -> ByteString
+requestSignature (Passport _ token) url qs headers =
+  encode $ url <> qs <> canonize headers
+  where encode = B64.encode . hmac hash 64 token
+        canonize = mconcat . map (uncurry mappend) . sortBy (comparing fst)
+
